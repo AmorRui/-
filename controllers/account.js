84734c4c1450430eb60477653db8ab8ac9843d8f
Amor <teng0009@126.com>
2018-03-17 19:33:46 +0800

    ':zap:成功注册'

--- controllers/account.js
+++ controllers/account.js
@@ -1,9 +1,7 @@
 /**
  * 账号控制器
  */
-const  models = require('../models/index')
-
-console.log(models)
+const { User } = require('../models')
 
 exports.login = (req, res) => {
   res.send('login')
@@ -14,19 +12,44 @@ exports.register = (req, res) => {
 }
 
 exports.registerPost = (req, res) => {
-  const { username, email, password,confirm,agree} = req.body
+  const { username, email, password, confirm, agree} = req.body
   // 表单不能为空
   if (!(username && email && password && confirm)) {
-    // throw new Error('请完整填写注册表单')
-    return res.render('register', { msg: '请完整填写注册表单' })
+    throw new Error('请完整填写注册表单')
   }
   // 密码是否一致
   if (password !== confirm) {
-    return res.render('register', { msg: '输入密码不一致' })
+    throw new Error('输入密码不一致')
   }
   // 必须同意协议
-  if (agree !== 'on') {
-    return res.render('register', { msg: '请同意协议' })
+  if (!agree) {
+    throw new Error('请同意协议')
   }
-  // 验证用户名字时候存在
+  // 判断用户时候存在
+User.findOne( { where: { username } } )
+  .then( user => {
+    if ( user ) throw new Error ('用户名已经存在')
+    return User.findOne( { where: { user_email: email } } )
+  })
+  .then( user => {
+    if ( user ) throw new Error ('邮箱已经存在')
+    // 持久化 
+    const newUser = new User()
+    newUser.username = username 
+    newUser.user_email = email
+
+    newUser.create_time = Date.now() / 1000
+    newUser.update_time = Date.now() / 1000
+    return newUser.save()
+     
+  })
+  .then ( user => {
+    // user => 新建过后的用户信息(包含ID和那些默认值)
+    console.log(user)
+    if( !user.user_id ) throw new Error('注册失败')
+    res.send('ok')
+  })
+  .catch( e => {
+    res.render('register',{ msg: e.message })
+  })
 }
--- models/index.js
+++ models/index.js
@@ -1,17 +1,25 @@
 const path = require('path')
+
+// 自动匹配路径
 const glob = require('glob')
-const Sequelize = require('sequelize');
+// ORM 框架
+const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  database: 'myshop',
+  host: '127.0.0.1',
+  database: 'newshop',
   username: 'root',
-  password: ''
+  password: '',
+  logging: false,
+  define: {
+    timestamps: false
+  }
 })
 
-glob.sync('*.js',{ cwd:__dirname})
-  .filter( item => item !== 'index.js')
-  .forEach (item => {
-    const model =  sequelize.import( path.join(__dirname,item ))
-    exports [model.nyame] = model 
+glob.sync('*.js', { cwd: __dirname })
+  .filter(item => item !== 'index.js')
+  .forEach(item => {
+    const model = sequelize.import(path.join(__dirname, item))
+    exports[model.name] = model
   })
